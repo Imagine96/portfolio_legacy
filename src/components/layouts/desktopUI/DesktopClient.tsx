c5f1a3f2a056ef8a5d187c86bd51e9d06e7d87ed
Ruben Guedes <imagine@macbook-air-de-ruben.home>
2022-02-10 02:19:00 +0100

    desktop0.1

--- src/components/layouts/desktopUI/DesktopClient.tsx
+++ src/components/layouts/desktopUI/DesktopClient.tsx
@@ -3,7 +3,6 @@ import Notes from "../../uiComponents/desktop/note/Notes";
 import Contact from "../../uiComponents/desktop/contact/Contact";
 import Hero from "../../uiComponents/desktop/hero/Hero";
 import Book from "../../uiComponents/desktop/book/Book";
-import Proyects from "../../uiComponents/desktop/proyects/Proyects";
 
 const DesktopClient: React.FC = () => {
   return (
@@ -13,7 +12,6 @@ const DesktopClient: React.FC = () => {
         <div className="w-full overlay space-y-8 relative backdrop-blur-md bg-ink bg-opacity-25 shadow-inner z-0">
           <Hero />
           <Book />
-          <Proyects />
           <Contact />
         </div>
       </main>
--- src/components/uiComponents/desktop/note/Note.tsx
+++ src/components/uiComponents/desktop/note/Note.tsx
@@ -3,15 +3,23 @@ import classes from "./Notes.module.css";
 
 interface Props {
   children?: React.ReactNode;
+  delay: number
   type?: "yellow" | "pink" | "white";
   rotation?: string;
   position?: string;
-  spacing?: string
+  spacing?: string;
 }
 
 const Note: React.FC<Props> = (props: Props) => {
 
   let noteClass = classes.yellowCard;
+  let animationDelay : string
+
+  if(props.delay < 2){
+    animationDelay = (props.delay).toString() + "s"
+  }else{
+    animationDelay = (props.delay + 2).toString() + "s"
+  }
 
   let spacing = "space-y-4"
 
@@ -29,8 +37,14 @@ const Note: React.FC<Props> = (props: Props) => {
     noteClass = classes.whiteCard;
   }
 
+  console.log(animationDelay)
+
   return (
     <div
+      style={{
+        opacity: 0,
+        animation: `intro 1s ease-out ${animationDelay} forwards`
+      }}
       className={` ${props.position} ${props.rotation} hidden md:flex z-50 absolute xl:h-[23rem] xl:w-[23rem] lg:h-[15rem] lg:w-[15rem] w-[14rem] h-[13rem] flex-col items-center justify-center shadow-md rounded-[32px] 
       ${noteClass}
       `}
