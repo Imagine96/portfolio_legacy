91310bafe234624580f7e83a237095cb0f867ce1
Ruben Guedes <imagine@macbook-air-de-ruben.home>
2022-02-18 05:08:51 +0100

    debug

--- src/components/layouts/desktopUI/DesktopClient.tsx
+++ src/components/layouts/desktopUI/DesktopClient.tsx
@@ -9,7 +9,7 @@ const DesktopClient: React.FC = () => {
   return (
     <>
       <Notes />
-      <main className="w-full overflow-hidden">
+      <main className="w-full desktop overflow-hidden">
         <div className="w-full overlay overflow-hidden 2xl:mx-auto space-y-8 relative pb-10 backdrop-blur-md bg-ink bg-opacity-25 shadow-inner z-0">
           <Hero />
           <Book />
